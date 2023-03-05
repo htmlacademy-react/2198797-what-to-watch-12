69e60730cd5564ba3b818ca0907f787e27ff9b96
Vitalii Beliaev <vtaliy.belyaev@gmail.com>
2023-03-05 19:20:41 +0800

    Создал компоненты страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,16 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppScreenProps = {
+  movieTitle: string;
+  genre: string;
+  releaseDate: string;
+}
+
+
+function App({movieTitle, genre, releaseDate}: AppScreenProps): JSX.Element {
+  return (
+    <MainPage movieTitle = {movieTitle} genre = {genre} releaseDate = {releaseDate}/>
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const mainScreenMovie = {
+  movieTitle: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  releaseDate: '2014'
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App movieTitle = {mainScreenMovie.movieTitle} genre = {mainScreenMovie.genre} releaseDate = {mainScreenMovie.releaseDate} />
   </React.StrictMode>,
 );
