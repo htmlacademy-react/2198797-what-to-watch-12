a06b74c177c374bf18a87a4afaddb21669b346b9
Vitalii Beliaev <vtaliy.belyaev@gmail.com>
2023-03-19 22:14:09 +0800

    Подправил форматировние

--- project/src/types/movie.ts
+++ project/src/types/movie.ts
@@ -1,21 +1,21 @@
 export type MovieDescription = {
-    name: string;
-    posterImage: string;
-    previewImage: string;
-    backgroundImage: string;
-    backgroundColor: string;
-    description: string;
-    rating: number;
-    scoresCount: number;
-    director: string;
-    starring: string[];
-    runTime: number;
-    genre: string;
-    released: number;
-    id: number;
-    isFavorite: boolean;
-    videoLink: string;
-    previewVideoLink: string;
+      name: string;
+      posterImage: string;
+      previewImage: string;
+      backgroundImage: string;
+      backgroundColor: string;
+      description: string;
+      rating: number;
+      scoresCount: number;
+      director: string;
+      starring: string[];
+      runTime: number;
+      genre: string;
+      released: number;
+      id: number;
+      isFavorite: boolean;
+      videoLink: string;
+      previewVideoLink: string;
   }
 
 export type ReviewDescription ={
